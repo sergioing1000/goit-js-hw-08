31c27fd219adc343dd9d0f989e5d6d176da8c81c
Sergio <114320133+sergioing1000@users.noreply.github.com>
2023-05-08 02:03:22 -0500

    modified:   package-lock.json 	modified:   package.json 	modified:   src/css/01-gallery.css 	modified:   src/js/01-gallery.js 	modified:   src/js/02-video.js

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -3,3 +3,39 @@ import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+// Descrito en la documentación
+import SimpleLightbox from "simplelightbox";
+// Importación adicional de estilos
+import "simplelightbox/dist/simple-lightbox.min.css";
+
+
+// ⬇️⬇️⬇️ Following lines create the gallery using a for each loop in the imported array coming from gallery-items.js file' On top of that these lines add classes to include styles alreedy defined in the styles.css file.
+
+const uList = document.querySelector("ul.gallery");
+
+galleryItems.forEach(array_element => {
+  
+  const liItem = document.createElement("li");
+  uList.append(liItem);
+  liItem.classList.add("gallery__item");
+
+  let newAnchor = document.createElement("a");
+  liItem.append(newAnchor);
+  newAnchor.href = array_element.original;
+  newAnchor.classList.add("gallery__link");
+
+  let newImg = document.createElement("img");
+  newImg.src = array_element.preview;
+  newImg.alt = array_element.description;
+  newAnchor.append(newImg);
+  newImg.classList.add("gallery__image");
+
+});
+
+
+let lightbox = new SimpleLightbox('.gallery a', { /* options */ });
